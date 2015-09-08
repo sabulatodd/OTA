5d94f502d603761ba966714cb810e99c8f99e931
Todd Papke <sabulatodd@mac.com>
2015-09-08 11:07:35 -0500

    Initial Commit

---
+++ OTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTA
+//
+//  Created by Todd Papke on 9/8/15.
+//  Copyright (c) 2015 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
