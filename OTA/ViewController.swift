063af63fbb161eba2ed31b6e374585c05981d6b9
Todd Papke <sabulatodd@mac.com>
2015-09-09 19:19:30 -0500

    Just Todd

--- OTA/ViewController.swift
+++ OTA/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
             view.addSubview(bgImage)
             //  view.sendSubviewToBack(self.bgImage)
         }
-        // Do any additional setup after loading the view, typically from a nib. Todd wasn't here   
+        // Do any additional setup after loading the view, typically from a nib. Todd
     }
 
     override func didReceiveMemoryWarning() {
