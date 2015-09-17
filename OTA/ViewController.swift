2ab1c5e9d7591d1a0da1bf2f6695a8c1a8551b87
Todd Papke <sabulatodd@mac.com>
2015-09-17 14:30:23 -0500

    Update to Swift 2

--- OTA/ViewController.swift
+++ OTA/ViewController.swift
@@ -10,20 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     //  @IBOutlet weak var bgImage: UIImageView!
+    
 
+    @IBOutlet weak var launchLogo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        if let image = UIImage(named:"blackOTALogo"){
-            print("hi Todd")
-            let bgImage = UIImageView(image: image)
-             bgImage.alpha = 0.2
+        //  if let image = UIImage(named:"blackOTALogo"){
+            print("hi Todd", terminator: "")
+            //let bgImage = UIImageView(image: image)
+             launchLogo.alpha = 0.2
             // bgImage.frame = self.view.bounds
-               bgImage.frame = CGRect(x: 0, y: 50, width: 370, height: 200)
-            view.addSubview(bgImage)
-            //  view.sendSubviewToBack(self.bgImage)
-        }
+            //  bgImage.frame = CGRect(x: 0, y: 50, width: 370, height: 200)
+        //    view.addSubview(bgImage)
+               //  view.sendSubviewToBack(self.bgImage)
+        // }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
